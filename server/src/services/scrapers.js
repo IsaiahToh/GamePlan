383c69d06a6062ce7a2d0e37be9efa7f6a791853
Nicholas Tan <nicktantk@gmail.com>
2025-06-17 17:34:56 +0800

    edit minor utils

--- server/src/services/scrapers.js
+++ server/src/services/scrapers.js
@@ -78,13 +78,13 @@ const LESSON_TYPE_MAP = {
   SEC: "Sectionals",
 };
 const dayOrder = [
+  "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
   "Friday",
   "Saturday",
-  "Sunday",
 ];
 function parseLesson(lessonStr) {
   const parts = lessonStr.split("-");
