6fd42c4bc02f8667ccf1e39f95e1029f375ee6e1
IsaiahToh <isaiahtohzhiming@gmail.com>
2025-07-16 20:37:33 +0800

    backend deployment errors 5

--- server/src/services/scrapers.js
+++ server/src/services/scrapers.js
@@ -2,9 +2,7 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 
 async function scrapeLessonClassNamesInKRfrugFP(url) {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
+  const browser = await puppeteer.launch({executablePath: '/path/to/Chrome'});
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
 
